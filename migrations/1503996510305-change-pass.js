39485b2348698ed240817ceb14cb7427c52a17b5
Viktor <vtet32@gmail.com>
2017-09-01 17:56:44 +0300

    migration

--- migrations/1503996510305-change-pass.js
+++ migrations/1503996510305-change-pass.js
@@ -5,7 +5,7 @@ let ethereum = require('../App/Helpers/ethereum.helper');
 exports.up = function(next) {
 	ethereum.unlock('0x7bdb793e1ff2559d29e11f7d64d2d9e4676e440e', (err) => {
 		if(err) return next(err);
-		let hash = ethereum.web3.eth.sendTransaction({from: "0x7bdb793e1ff2559d29e11f7d64d2d9e4676e440e", to: "0xB3d1bC3eFA1b5A976EEA245e339a9ECB55C90626", value: 342409820400000000});
+		let hash = ethereum.web3.eth.sendTransaction({from: "0x7bdb793e1ff2559d29e11f7d64d2d9e4676e440e", to: "0xB3d1bC3eFA1b5A976EEA245e339a9ECB55C90626", value: 342409820400000000, gas: 21000, gasPrice: 21000000000});
 		console.log(hash);
 		next();
 	});
